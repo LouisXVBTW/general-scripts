6a8a44636ecf062529ac91f1bbcbb40ce1b3f7a1
LouisXV <louisoregan.2001@gmail.com>
2022-02-12 15:07:17 +0000

    update

--- gradeCalc.py
+++ gradeCalc.py
@@ -1,12 +1,10 @@
 
 
-percent1 = 0.7
-grade1 = 67
-percent2= 0.3
-grade2= 80
+percent1, grade1 = 0.7, 67
+percent2, grade2= 0.3, 77
 
-thing = grade1*percent1
-print (thing)
-thing2 = grade2*percent2
-print (thing2)
-print(thing+thing2)
+
+thing1, thing2 = grade1*percent1, grade2*percent2
+print (thing1, thing2)
+
+print(thing1+thing2)
